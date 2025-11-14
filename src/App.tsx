9963a02607a9e563a84e49ae32b8601448d7af5b
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-11-14 14:25:56 +0700

    change to create browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import AppRoutes from "./routes/AppRoutes"
-function App() {
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/router';
 
-  return (
-    <>
-    <AppRoutes/>
-    </>
-  )
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App
